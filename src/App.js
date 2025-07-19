46430429e8fab27159d62aee15dc2a477182ec1d
QT-Tech <nguyenquangtoank5@gmail.com>
2025-07-19 20:51:07 +0700

    setup bootstrap & react router dom

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
-import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
-const App = () => {
-  const count = useSelector((state) => state.counter.count);
-  const dispatch = useDispatch();
+import "./App.scss";
 
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World!</p>
-        <div>Count = {count}</div>
-        <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-        <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-      </header>
+    <div className="app-container">
+      hello world
+      <button>test</button>
     </div>
   );
 };
