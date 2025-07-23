a181a4af51ac028ca6bb7ae95aa507d3cde969d2
QT-Tech <nguyenquangtoank5@gmail.com>
2025-07-23 14:53:11 +0700

    design homepage

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+// import NavDropdown from "react-bootstrap/NavDropdown";
 
 const Header = () => {
   return (
@@ -25,11 +25,13 @@ const Header = () => {
             </NavLink>
           </Nav>
           <Nav>
-            <NavDropdown title="Settings" id="basic-nav-dropdown">
+            <button className="btn-login">Log in</button>
+            <button className="btn-signup">Sign up</button>
+            {/* <NavDropdown title="Settings" id="basic-nav-dropdown">
               <NavDropdown.Item>Log in</NavDropdown.Item>
               <NavDropdown.Item>Log out</NavDropdown.Item>
               <NavDropdown.Item>Profile</NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,5 +1,23 @@
+import videoHomepage from "../../assets/video-homepage.mp4";
+
 const HomePage = (props) => {
-  return <div className="homepage-container">home page compponent</div>;
+  return (
+    <div className="homepage-container">
+      <video autoPlay muted loop>
+        <source src={videoHomepage} type="video/mp4" />
+      </video>
+      <div className="homepage-content">
+        <div className="homepage-text">There's a better way to ask</div>
+        <div className="homepage-title">
+          You don't want to make a boring form. And your audience won'n answer
+          one. Create a typeform instead - and make everyone happy.
+        </div>
+        <div className="homepage-button">
+          <button>Get's started. It's free</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
