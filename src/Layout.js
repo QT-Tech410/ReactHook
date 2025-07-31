c3d02863661fede6e0a2083a6904601490b0d0c2
QT-Tech <nguyenquangtoank5@gmail.com>
2025-07-31 08:52:04 +0700

    APIs Login Form

---
+++ src/Layout.js
@@ -0,0 +1,43 @@
+import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import App from "./App";
+import User from "./components/User/User";
+import Admin from "./components/Admin/Admin";
+import HomePage from "./components/Home/HomePage";
+import DashBoard from "./components/Admin/Content/DashBoard";
+import ManageUser from "./components/Admin/Content/ManageUser";
+import Login from "./components/Admin/Auth/Login";
+
+const Layout = (props) => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomePage />} />
+          <Route path="users" element={<User />} />
+        </Route>
+        <Route path="/admins" element={<Admin />}>
+          <Route index element={<DashBoard />} />
+          <Route path="manage-users" element={<ManageUser />} />
+        </Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+    </>
+  );
+};
+
+export default Layout;
---
+++ src/components/Admin/Auth/Login.js
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import "./Login.scss";
+import { useNavigate } from "react-router-dom";
+import { postLogin } from "../../../services/apiService";
+import { toast } from "react-toastify";
+
+const Login = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async () => {
+    //validate
+
+    //submit apis
+    let data = await postLogin(email, password);
+    if (data && data.EC === 0) {
+      toast.success(data.EM);
+      navigate("/");
+    }
+
+    if (data && data.EC !== 0) {
+      toast.error(data.EM);
+    }
+  };
+  return (
+    <div className="login-container">
+      <div className="header">
+        <span>Don't have an account yet?</span>
+        <button>Sign up</button>
+      </div>
+      <div className="title col-4 mx-auto">QT-TECH</div>
+      <div className="welcome col-4 mx-auto">Hello, who's this?</div>
+      <div className="content-form col-4 mx-auto">
+        <div className="form-group">
+          <label>Email</label>
+          <input
+            type={"email"}
+            className="form-control"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            type={"password"}
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            className="form-control"
+          />
+        </div>
+        <span className="forgot-password">Forgot Password ?</span>
+        <div>
+          <button onClick={() => handleLogin()} className="btn-submit">
+            Login
+          </button>
+        </div>
+        <div className="text-center">
+          <span
+            className="back"
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            &#60;&#60; Go to Homepage
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
