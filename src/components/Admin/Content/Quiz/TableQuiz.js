9035a69c66a6bda4a24802017bd70a71430e8b1d
QT-Tech <nguyenquangtoank5@gmail.com>
2025-08-04 10:36:10 +0700

    Display List Quiz For Admin

---
+++ src/components/Admin/Content/Quiz/TableQuiz.js
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import { getAllQuizForAdmin } from "../../../../services/apiService";
+
+const TableQuiz = (props) => {
+  const [listQuiz, setListQuiz] = useState([]);
+
+  useEffect(() => {
+    fetchQuiz();
+  }, []);
+
+  const fetchQuiz = async () => {
+    let res = await getAllQuizForAdmin();
+    if (res && res.EC === 0) {
+      setListQuiz(res.DT);
+    }
+  };
+  return (
+    <>
+      <div>List Quizzes: </div>
+      <table className="table table-hover table-bordered mt-2">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Description</th>
+            <th scope="col">Type</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listQuiz &&
+            listQuiz.map((item, index) => {
+              return (
+                <tr key={`table-quiz-${index}`}>
+                  <td>{item.id}</td>
+                  <td>{item.name}</td>
+                  <td>{item.description}</td>
+                  <td>{item.difficulty}</td>
+                  <td>
+                    <button
+                      className="btn btn-warning mx-3"
+                      //   onClick={() => props.handleClickBtnUpdate(item)}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      className="btn btn-danger"
+                      //   onClick={() => props.handleClickBtnDelete(item)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default TableQuiz;
--- src/services/apiService.js
+++ src/services/apiService.js
@@ -71,6 +71,10 @@ const postCreateNewQuiz = (description, name, difficulty, image) => {
   return axios.post("api/v1/quiz", data);
 };
 
+const getAllQuizForAdmin = () => {
+  return axios.get(`api/v1/quiz/all`);
+};
+
 export {
   postCreateNewUser,
   getAllUsers,
@@ -83,4 +87,5 @@ export {
   getDataQuiz,
   postSubmitQuiz,
   postCreateNewQuiz,
+  getAllQuizForAdmin,
 };
