d9a31af56b5b664981c78364be198b04d0c1fd19
QT-Tech <nguyenquangtoank5@gmail.com>
2025-08-10 21:13:47 +0700

    Private route

--- src/components/User/Content/RightContent.js
+++ src/components/User/Content/RightContent.js
@@ -22,6 +22,7 @@ const RightContent = (props) => {
   };
 
   const handleClickQuestion = (index, question) => {
+    props.setIndex(index);
     if (refDiv.current) {
       refDiv.current.forEach((item) => {
         if (item && item.className === "question clicked") {
@@ -38,7 +39,6 @@ const RightContent = (props) => {
     }
 
     refDiv.current[index].className = "question clicked";
-    props.setIndex(index);
   };
 
   return (
